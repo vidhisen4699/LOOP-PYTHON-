6f1e0eb9785d4570c28bdbb836918f5d3d577514
VIDHI SEN <100829319+vidhisen4699@users.noreply.github.com>
2022-03-04 10:48:03 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,36 @@
+"""n='python'
+i=0
+sum=' '
+while <=n:
+    i=i+1
+    #sum=sum+1
+print(i)    
+"""
+
+"""list=[4,5,-6,-7,9,-11,12,-14]
+i=0
+while i<len(list):
+    if list[i]==-i:
+        print('0')
+    i=i+1
+print(list[i])         """
+
+
+
+
+list=['kirti','sheetal','kartic','arti','vidhi']
+l=[]
+i=0
+while i<len(list):
+    if list[i]=='a':
+        l.append(i)
+    i=i+1
+print(l)        
+
+
+
+"""l=[12,45,15,17,23]
+for i,j in l:
+    c=i+j
+print(c)
+"""
